a068d4c38829912a16035283d0d8730992528306
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-08-14 13:24:32 +0300

    our first endpoint on node!

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+app.use(function(req, res, next){
+  res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-{random}' 'strict-dynamic' https: http:;base-uri 'none';report-uri:/csp-report");
+  next()
+})
+
 app.get('/', function(req, res){
   res.send(req.params)
+})
+
+app.post('/csp-report', function(req, res){
+  res.send(req.params)
+})
+
+app.get('/noNonce', function(req, res){
+  res.sendFile(__dirname + '/scriptwithoutnonce.html')
 })
