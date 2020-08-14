4a408616f929233eb7cee162ce63ee4bf07c6944
giannisc <giannischatziveroglou@gmail.com>
2020-08-14 15:39:04 +0300

    implement last 2 csp violations

--- app.js
+++ app.js
@@ -6,24 +6,23 @@ const bodyParser = require('body-parser');
 var app = express()
 
 app.use(bodyParser.json({type: 'application/csp-report'}));
+
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 app.use(function(req, res, next){
-    res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-1234' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-reports");
+    res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-r4nd0m' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-reports");
     next()
 })
-
-app.use(bodyParser.raw())
 
 app.get('/', function(req, res){
     res.send(req.params)
 })
 
-app.post('/csp-report', function(req, res){
+app.post('/csp-reports', function(req, res){
   console.log('CSP violation!')
-  console.log(req.body)
+//   console.log(req.body)
   res.sendStatus(204)
 })
 
@@ -33,4 +32,8 @@ app.get('/noNonce', function(req, res){
 
 app.get('/jsUri', function(req, res){
     res.sendFile(__dirname + '/jsUri.html')
+})
+
+app.get('/unsafeEval', function(req, res){
+    res.sendFile(__dirname + '/unsafeEval.html')
 })
