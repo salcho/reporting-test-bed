e156743e71b9aaf2935cef4bd38629857065c035
giannisc <giannischatziveroglou@gmail.com>
2020-08-14 13:42:01 +0300

    adding jsUri case endpoint

--- app.js
+++ app.js
@@ -6,26 +6,30 @@ const bodyParser = require('body-parser');
 var app = express()
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 app.use(function(req, res, next){
-  res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-1234' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-report");
-  next()
+    res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-1234' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-report");
+    next()
 })
 
 app.use(bodyParser.raw())
 
 app.get('/', function(req, res){
-  res.send(req.params)
+    res.send(req.params)
 })
 
 app.post('/csp-report', function(req, res){
-  console.log('CSP violation!')
-  console.log(req)
-  res.sendStatus(204)
+    console.log('CSP violation!')
+    console.log(req)
+    res.sendStatus(204)
 })
 
 app.get('/noNonce', function(req, res){
-  res.sendFile(__dirname + '/scriptwithoutnonce.html')
+    res.sendFile(__dirname + '/scriptwithoutnonce.html')
+})
+
+app.get('/jsUri', function(req, res){
+    res.sendFile(__dirname + '/jsUri.html')
 })
