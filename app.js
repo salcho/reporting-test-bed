a55586084137d11dddda9e6a3a35576d9d38e58e
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-08-14 13:38:56 +0300

    Fixed typo on CSP header, trying to read csp reports

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
-var express = require('express')
+const express = require('express')
+const bodyParser = require('body-parser');
 var app = express()
 
 app.listen(port, hostname, () => {
@@ -11,16 +10,20 @@ app.listen(port, hostname, () => {
 });
 
 app.use(function(req, res, next){
-  res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-{random}' 'strict-dynamic' https: http:;base-uri 'none';report-uri:/csp-report");
+  res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-1234' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-report");
   next()
 })
+
+app.use(bodyParser.raw())
 
 app.get('/', function(req, res){
   res.send(req.params)
 })
 
 app.post('/csp-report', function(req, res){
-  res.send(req.params)
+  console.log('CSP violation!')
+  console.log(req)
+  res.sendStatus(204)
 })
 
 app.get('/noNonce', function(req, res){
