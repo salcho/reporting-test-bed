06ad8bec25e2e99e11d865e316105e305f9be893
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-08-14 13:12:23 +0300

    Using express in app.js

--- app.js
+++ app.js
@@ -6,17 +6,10 @@ const port = 3000;
 var express = require('express')
 var app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-{random}' 'strict-dynamic' https: http:;base-uri 'none';report-uri csp-report");
-  // res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-app.get('/here', function(req, res){
+app.get('/', function(req, res){
   res.send(req.params)
 })
