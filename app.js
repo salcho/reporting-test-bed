48c00f172abb6fb6897ecbf23c59c3f19da01b9e
giannisc <giannischatziveroglou@gmail.com>
2020-08-14 15:39:04 +0300

    implement last 2 csp violations

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port, hostname, () => {
 });
 
 app.use(function(req, res, next){
-    res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-1234' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-reports");
+    res.setHeader('Content-Security-Policy', "object-src 'none';script-src 'nonce-r4nd0m' 'strict-dynamic' https: http:;base-uri 'none'; report-uri /csp-reports");
     next()
 })
 
