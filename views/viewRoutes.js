91c5850cae6c01c0c9ba72258d8051b4692b0b35
Giannis Chatziveroglou <gchatz@mit.edu>
2020-08-21 12:17:30 +0300

    Organizing file structure

--- views/viewRoutes.js
+++ views/viewRoutes.js
@@ -3,46 +3,46 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/csp/scriptWithoutNonce', function(req, res){
-  res.sendFile(__dirname + '/scriptwithoutnonce.html')
+  res.sendFile(__dirname + '/csp/scriptwithoutnonce.html')
 })
 
 router.get('/csp/inlineEventHandler', function(req, res){
-  res.sendFile(__dirname + '/inlineEventHandler.html')
+  res.sendFile(__dirname + '/csp/inlineEventHandler.html')
 })
 
 router.get('/csp/jsUri', function(req, res){
-    res.sendFile(__dirname + '/jsUri.html')
+    res.sendFile(__dirname + '/csp/jsUri.html')
 })
 
 router.get('/csp/unsafeEval', function(req, res){
-  res.sendFile(__dirname + '/unsafeEval.html')
+  res.sendFile(__dirname + '/csp/unsafeEval.html')
 })
 
 router.get('/trustedTypes/innerHTML', (req, res) => {
-  res.sendFile(__dirname + '/innerHtmlTT.html')
+  res.sendFile(__dirname + '/trustedTypes/innerHtmlTT.html')
 })
 
 router.get('/trustedTypes/scriptSrc', (req, res) => {
-  res.sendFile(__dirname + '/scriptSrcTT.html')
+  res.sendFile(__dirname + '/trustedTypes/scriptSrcTT.html')
 })
 
 router.get('/coep/test', (req, res) => {
   // res.setHeader('Reporting-Endpoints', 'coep-endpoint="https://localhost:3443/coep-reports"')
   // res.setHeader("Report-To", "{ group: 'coep-endpoint', max_age: 86400, endpoints: [{ url: 'https://localhost:3443/coep-reports'}]}")
   res.setHeader("Cross-Origin-Embedder-Policy", 'require-corp; report-to="coep-endpoint"')
-  res.sendFile(__dirname + '/coepTest.html')
+  res.sendFile(__dirname + '/coep/coepTest.html')
 })
 
 router.get('/coep/crossOriginIframe', (req, res) => {
-  res.sendFile(__dirname + '/crossOriginIframe.html')
+  res.sendFile(__dirname + '/coep/crossOriginIframe.html')
 })
 
 router.get('/coep/crossOriginResources', (req, res) => {
-  res.sendFile(__dirname + '/crossOriginResources.html')
+  res.sendFile(__dirname + '/coep/crossOriginResources.html')
 })
 
 router.get('/coep/crossOriginPopup', (req, res) => {
-  res.sendFile(__dirname + '/crossOriginPopup.html')
+  res.sendFile(__dirname + '/coep/crossOriginPopup.html')
 })
 
 module.exports = router;
